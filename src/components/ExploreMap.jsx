9f8343f704d7fdbcd03ae20b071e75a1333bd4db
Saur-Deshmukh <saur90044@gmail.com>
2025-01-31 07:33:48 +0530

    new map

--- src/components/ExploreMap.jsx
+++ src/components/ExploreMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import map from '/Techtopia.svg';
 
 export function ExploreMap() {
   return (
@@ -8,8 +8,12 @@ export function ExploreMap() {
         <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
           Techopia!
         </span>
-        <img src="map.svg" alt="" className='m-auto -rotate-90 h-[1200px]'/>
       </h2>
+      <div className="flex justify-center">
+        <object id="map" data={map} type="image/svg+xml" className="max-w-full h-auto">
+          <img src={map} alt="map" className="max-w-full h-auto" />
+        </object>
+      </div>
     </section>
   );
 }
